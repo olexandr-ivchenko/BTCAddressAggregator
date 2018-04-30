046de94acfd050d7fdb84abe5003e39001685519
oivchenko <oivchenko@luxoft.com>
2018-05-01 00:38:22 +0300

    #3 added unit tests to ensure that parsing transactions and block are working correctly.

---
+++ src/test/java/com/olexandrivchenko/bitcoinkiller/database/main/BlockToAddressConverterTest.java
@@ -0,0 +1,57 @@
+package com.olexandrivchenko.bitcoinkiller.database.main;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.olexandrivchenko.bitcoinkiller.database.inbound.jsonrpc.Block;
+import com.olexandrivchenko.bitcoinkiller.database.inbound.jsonrpc.Tx;
+import com.olexandrivchenko.bitcoinkiller.database.outbound.dto.Address;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlockToAddressConverterTest {
+
+    @Autowired
+    BlockToAddressConverter converter;
+
+    /**
+     * Please refer to https://github.com/olexandr-ivchenko/btcKillerDatabase/issues/3
+     * block height 118398
+     * transactionId 9056cc7c2181dbd1d2ec6bd92b3d7628f1811b898b009895edfd587e6c5c0cb3
+     * Dumped to file 118398_9056cc7c2181dbd1d2ec6bd92b3d7628f1811b898b009895edfd587e6c5c0cb3.json
+     */
+    @Test
+    public void testIssue3Transaction() throws IOException {
+        String jsonBody = IOUtils.toString(
+                this.getClass().getResourceAsStream("/com/olexandrivchenko/bitcoinkiller/database/blocks/118398.json"),
+                "UTF-8");
+        ObjectMapper mapper = new ObjectMapper();
+        Block block = mapper.readValue(jsonBody, Block.class);
+        List<Address> result = converter.convert(block);
+
+        assertEquals(110, result.size());
+        double sum = 0;
+        for (Address addr : result) {
+            sum += addr.getAmount();
+        }
+        assertEquals(50, sum, 0.000000001);
+        Set<Address> expectedAddr = result.stream()
+                .filter(o -> o.getAddress().equals("184wu9LYeh8q73i9CHWNA8GR5CDZUMQeSw"))
+                .collect(Collectors.toSet());
+        assertEquals(1, expectedAddr.size());
+        assertEquals("184wu9LYeh8q73i9CHWNA8GR5CDZUMQeSw", expectedAddr.stream().findFirst().map(Address::getAddress).orElse(null));
+    }
+
+
+}
---
+++ src/test/java/com/olexandrivchenko/bitcoinkiller/database/main/TxToAddressConverterTest.java
@@ -0,0 +1,50 @@
+package com.olexandrivchenko.bitcoinkiller.database.main;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.olexandrivchenko.bitcoinkiller.database.inbound.BitcoindCallerImpl;
+import com.olexandrivchenko.bitcoinkiller.database.inbound.jsonrpc.Tx;
+import com.olexandrivchenko.bitcoinkiller.database.outbound.dto.Address;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TxToAddressConverterTest {
+
+    @Autowired
+    TxToAddressConverter converter;
+
+    /**
+     * Please refer to https://github.com/olexandr-ivchenko/btcKillerDatabase/issues/3
+     * block height 118398
+     * transactionId 9056cc7c2181dbd1d2ec6bd92b3d7628f1811b898b009895edfd587e6c5c0cb3
+     * Dumped to file 118398_9056cc7c2181dbd1d2ec6bd92b3d7628f1811b898b009895edfd587e6c5c0cb3.json
+     */
+    @Test
+    public void testIssue3Transaction() throws IOException {
+        String jsonBody = IOUtils.toString(
+                this.getClass().getResourceAsStream("/com/olexandrivchenko/bitcoinkiller/database/transactions/118398_9056cc7c2181dbd1d2ec6bd92b3d7628f1811b898b009895edfd587e6c5c0cb3.json"),
+                "UTF-8");
+        ObjectMapper mapper = new ObjectMapper();
+        Tx tx = mapper.readValue(jsonBody, Tx.class);
+        List<Address> result = converter.convert(tx);
+
+        assertEquals(14, result.size());
+        double sum = 0;
+        for (Address addr : result) {
+            sum += addr.getAmount();
+        }
+        assertEquals(0, sum, 0.000000001);
+        assertEquals("184wu9LYeh8q73i9CHWNA8GR5CDZUMQeSw", result.get(13).getAddress());
+    }
+
+}
